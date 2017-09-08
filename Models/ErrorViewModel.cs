2049395d0f0b1cc697d3671b01a7dc9ea985a02f
Mateo Escobedo <mat.esc@outlook.com>
2017-09-08 19:34:34 -0400

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetcore_docker_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
